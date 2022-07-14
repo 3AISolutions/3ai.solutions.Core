8ca41f7749689804cc1281462458f51cefacee2f
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-07-15 02:30:58 +0300

    init

---
+++ Interfaces/IStorageService.cs
@@ -0,0 +1,12 @@
+﻿namespace _3ai.solutions.Core.Interfaces
+{
+    public interface IStorageService
+    {
+        public Task<byte[]> GetAsync(string remotePath, CancellationToken token = default);
+        public Task<bool> DeleteAsync(string remotePath, CancellationToken token = default);
+        public Task<string> AddAsync(string relativePath, byte[] data, string contentType, CancellationToken token = default);
+        public string GetURI(string remotePath);
+        public string GetAccessURL(string remotePath, string ip);
+        public Task<bool> ZipAsync(string remotePaths, CancellationToken token = default);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# 3ai.solutions.Core
