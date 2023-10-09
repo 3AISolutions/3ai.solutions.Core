132afcc84080b7f03875af6e4c2cc15faf20d841
Andreas Michaelou <andreas@belugga.io>
2023-10-09 10:39:37 +0300

    result reponse in SMS

--- Interfaces/ISMSService.cs
+++ Interfaces/ISMSService.cs
@@ -1,9 +1,10 @@
+using _3ai.solutions.Core.Models;
 namespace _3ai.solutions.Core.Interfaces
 {
     public interface ISMSService
     {
-        public Task<bool> SendSMSAsync(string message, string number, CancellationToken token = default);
-        public Task<bool> SendSMSAsync(string message, string number, string senderId, CancellationToken token = default);
-        public Task<bool> SendSMSAsync(string message, string number, string senderId, string smsType, CancellationToken token = default);
+        public Task<Result> SendSMSAsync(string message, string number, CancellationToken token = default);
+        public Task<Result> SendSMSAsync(string message, string number, string senderId, CancellationToken token = default);
+        public Task<Result> SendSMSAsync(string message, string number, string senderId, string smsType, CancellationToken token = default);
     }
 }
---
+++ Models/Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ai.solutions.Core.Models
+{
+    public record Result
+    {
+        public bool Success { get; init; }
+        public string? Message { get; init; }
+        public string? LongMessage { get; init; }
+    }
+}
